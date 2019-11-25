dd9d6c28d8b1e51264c48fc0744f60d678d46fa4
lxr <mikerong@126.com>
2019-11-25 23:07:49 +0800

    第二次提交源代码 TestGit 增加class

---
+++ src/main/java/com/db/common/java/model/TestGit.java
@@ -0,0 +1,10 @@
+package com.db.common.java.model;
+
+/**
+ * 测试git 代码提交
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
